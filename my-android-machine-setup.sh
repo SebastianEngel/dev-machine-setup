f937eba30e672b8e12d9f79e06a435f856d7b99f
Sebastian Engel <engel.sebastian@gmail.com>
2016-11-16 09:59:27 +0100

    Adding Skype installation

--- my-android-machine-setup.sh
+++ my-android-machine-setup.sh
@@ -38,6 +38,7 @@ brew cask install spotify
 brew cask install appcleaner
 brew cask install evernote
 brew cask install whatsapp
+brew cask install skype
 
 echo "Open Tasks:"
 echo "- set up your ssh keys for repository access, etc."
