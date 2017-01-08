f1b03f0e5d69012766b677c669838c740c263714
Sebastian Engel <engel.sebastian@gmail.com>
2017-01-08 13:38:19 +0100

    Adding Sidestep installation

--- my-android-machine-setup.sh
+++ my-android-machine-setup.sh
@@ -45,6 +45,7 @@ brew cask install appcleaner
 brew cask install evernote
 brew cask install whatsapp
 brew cask install skype
+brew cask install sidestep
 
 echo "Open Tasks:"
 echo "- set up your ssh keys for repository access, etc."
